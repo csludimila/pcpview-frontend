f28d04a4b2b5e4adb5f4a20b1b1578628418e237
Ludimila <csludimila@gmail.com>
2026-04-30 15:07:30 -0300

    Merge branch 'main' of https://github.com/csludimila/projeto-pcpview-frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,10 @@
+
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { OrderFormComponent } from './components/order-form/order-form';
 import { MachineListComponent } from './components/machine-list/machine-list';
+import { OrderFormComponent } from './components/order-form/order-form';
 
 export const routes: Routes = [
-  { path: 'escritorio', component: OrderFormComponent }, // Tela do Admin
-  { path: 'fabrica', component: MachineListComponent },   // Tela do Operador
-  { path: '', redirectTo: '/fabrica', pathMatch: 'full' } // Página inicial padrão
+  { path: 'escritorio', component: OrderFormComponent },
+  { path: 'fabrica', component: MachineListComponent },
+  { path: '', redirectTo: '/escritorio', pathMatch: 'full' }
 ];
-=======
-
-export const routes: Routes = [];
->>>>>>> ccdc7bc7fb2ed023105334178c3e0e8634ce9a7b
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,35 +1,18 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// 1. Adicionamos RouterLink e RouterLinkActive aqui para os botões do menu funcionarem
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-
-// Os imports dos seus componentes continuam iguais
 import { OrderFormComponent } from './components/order-form/order-form';
 import { MachineListComponent } from './components/machine-list/machine-list';
-=======
-
-import { Component } from '@angular/core';
-
-import { OpFormComponent } from './components/op-form/op-form'; 
->>>>>>> ccdc7bc7fb2ed023105334178c3e0e8634ce9a7b
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
   imports: [
     CommonModule, 
     RouterOutlet, 
-    RouterLink,       // 2. Adicionado para suportar o link [routerLink] no HTML
-    RouterLinkActive, // 3. Adicionado para destacar o botão da página atual
-    OrderFormComponent, 
-    MachineListComponent
+    RouterLink, 
+    RouterLinkActive,
   ],
-=======
-  // Aqui damos permissão para o chefe usar o seu formulário
-  imports: [ OpFormComponent], 
->>>>>>> ccdc7bc7fb2ed023105334178c3e0e8634ce9a7b
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
